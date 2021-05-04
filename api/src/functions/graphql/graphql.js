d68a05d63c58599672053861620dc3d455919fec
John-Dag Addy <jaeadgy@gmail.com>
2021-05-04 17:03:28 +0000

    finally got the netlify auth working

--- api/src/functions/graphql/graphql.js
+++ api/src/functions/graphql/graphql.js
@@ -2,8 +2,9 @@
 // as a lambda function
 
 const { ApolloServer } = require('apollo-server-lambda')
-const { makeAugmentedSchema } = require('neo4j-graphql-js')
+const { makeAugmentedSchema, assertSchema } = require('neo4j-graphql-js')
 const neo4j = require('neo4j-driver')
+const jwt = require('jsonwebtoken')
 
 // This module is copied during the build step
 // Be sure to run `npm run build`
@@ -32,11 +33,51 @@ const schema = makeAugmentedSchema({
   },
 })
 
+assertSchema({ schema, driver, debug: true })
+
 const server = new ApolloServer({
-  schema: schema,
-  context: ({ event }) => {
-    return { req: event, driver, neo4jDatabase: process.env.NEO4J_DATABASE }
+  schema,
+  context: async ({ event }) => {
+    const token = event.headers?.authorization?.slice(7)
+    // let userId
+
+    if (!token) {
+      return {
+        driver,
+      }
+    }
+
+    const authResult = new Promise((resolve, reject) => {
+      jwt.verify(
+        token,
+        process.env.JWT_SECRET,
+        {
+          algorithms: ['RS256'],
+        },
+        (error, decoded) => {
+          if (error) {
+            reject({ error })
+          }
+          if (decoded) {
+            resolve(decoded)
+          }
+        }
+      )
+    })
+
+    const decoded = await authResult
+
+    return {
+      driver,
+      req: event,
+      cypherParams: {
+        userId: decoded.sub,
+      },
+      neo4jDatabase: process.env.NEO4J_DATABASE,
+    }
   },
+  // introspection: true,
+  // playground: true,
 })
 
 exports.handler = server.createHandler()

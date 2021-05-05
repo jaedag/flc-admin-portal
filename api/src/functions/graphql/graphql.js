418e6ce76a6125fc9e0aa4bbf14f829f49e20ac0
John-Dag Addy <jaeadgy@gmail.com>
2021-05-05 22:46:34 +0000

    another thing

--- api/src/functions/graphql/graphql.js
+++ api/src/functions/graphql/graphql.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken')
 
 // This module is copied during the build step
 // Be sure to run `npm run build`
-const { typeDefs } = require('./graphql-schema')
+const { typeDefs } = require('../graphql-schema')
 
 const driver = neo4j.driver(
   process.env.NEO4J_URI || 'bolt://localhost:7687',

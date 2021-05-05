f5c21d85204e3cff4398c2204ac832661410e801
John-Dag Addy <jaeadgy@gmail.com>
2021-05-05 23:38:11 +0000

    assert Schema

--- api/src/functions/graphql/graphql.js
+++ api/src/functions/graphql/graphql.js
@@ -2,7 +2,7 @@
 // as a lambda function
 
 const { ApolloServer } = require('apollo-server-lambda')
-const { makeAugmentedSchema } = require('neo4j-graphql-js')
+const { makeAugmentedSchema, assertSchema } = require('neo4j-graphql-js')
 const neo4j = require('neo4j-driver')
 const jwt = require('jsonwebtoken')
 
@@ -33,7 +33,7 @@ const schema = makeAugmentedSchema({
   },
 })
 
-// assertSchema({ schema, driver, debug: true })
+assertSchema({ schema, driver, debug: true })
 
 const server = new ApolloServer({
   schema,

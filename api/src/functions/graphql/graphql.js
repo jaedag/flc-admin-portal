3a64627b19d99264f63140bdea76f2cd3329d95c
John-Dag Addy <jaeadgy@gmail.com>
2021-05-07 18:34:20 +0000

    shorter solution

--- api/src/functions/graphql/graphql.js
+++ api/src/functions/graphql/graphql.js
@@ -4,7 +4,7 @@
 const { ApolloServer } = require('apollo-server-lambda')
 const { makeAugmentedSchema, assertSchema } = require('neo4j-graphql-js')
 const neo4j = require('neo4j-driver')
-const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
 
 // This module is copied during the build step
 // Be sure to run `npm run build`
@@ -36,44 +36,11 @@ const schema = makeAugmentedSchema({
 assertSchema({ schema, driver, debug: true })
 
 const server = new ApolloServer({
-  schema: schema,
-  context: async ({ event }) => {
-    const token = event.headers?.authorization?.slice(7)
-    // let userId
-
-    if (!token) {
-      return {
-        driver,
-      }
-    }
-
-    const authResult = new Promise((resolve, reject) => {
-      jwt.verify(
-        token,
-        process.env.JWT_SECRET,
-        {
-          algorithms: ['RS256'],
-        },
-        (error, decoded) => {
-          if (error) {
-            reject({ error })
-          }
-          if (decoded) {
-            resolve(decoded)
-          }
-        }
-      )
-    })
-
-    const decoded = await authResult
-
+  schema,
+  context: ({ event }) => {
     return {
       driver,
       req: event,
-      cypherParams: {
-        userId: decoded.sub,
-      },
-      neo4jDatabase: process.env.NEO4J_DATABASE,
     }
   },
   introspection: true,

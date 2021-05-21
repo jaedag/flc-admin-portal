d95d2c812f47c123d8be580b8fff941932ea892f
John-Dag Addy <jaeadgy@gmail.com>
2021-05-21 11:44:54 +0000

    implemented custom resolver for creating auth0 profile on Admin Mutation

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -3,6 +3,16 @@ const axios = require('axios').default
 
 dotenv.config()
 
+const matchMemberQuery = `MATCH (member:Member {id:$from.id}) MATCH (member)-[:IS_ADMIN_FOR|:LEADS]->() 
+  RETURN 
+  member.id AS id,
+  member.auth_id AS auth_id,
+  member.firstName AS firstName, 
+  member.lastName AS lastName,
+  member.emailAddress AS emailAddress,
+  member.phoneNumber AS phoneNumber,
+  member.pictureUrl AS pictureUrl`
+
 export const resolvers = {
   // Resolver Parameters
   // Object: the parent result of a previous resolver
@@ -24,6 +34,52 @@ export const resolvers = {
       const response = await axios(config)
 
       return response.data[0]?.identities[0]?.user_id
+    },
+  },
+  Mutation: {
+    AddMemberIsBishopAdminFor: async (object, args, context, resolveInfo) => {
+      const session = context.driver.session()
+
+      await session.run(matchMemberQuery, args).then((response) => {
+        let member = {}
+        response.records[0].keys.forEach(
+          (key, i) => (result[key] = response.records[0]._fields[i])
+        )
+
+        //Check if Member has Auth0 ID
+        //If not, create auth0 profile
+        if (!member.auth_id) {
+          const config = {
+            method: 'post',
+            url: `https://flcadmin.us.auth0.com/api/v2/users`,
+            headers: {
+              autho: '',
+              Authorization: `Bearer ${process.env.AUTH_API_TOKEN}`,
+            },
+            body: {
+              email: member.emailAddress,
+              phoneNumber: member.phoneNumber,
+              user_metadata: {
+                admin: true,
+              },
+              given_name: member.firstName,
+              family_name: member.lastName,
+              name: `${member.firstName} ${member.lastName}`,
+              picture: member.pictureUrl,
+              user_id: member.id,
+              password: 'rAndoMLetteRs',
+            },
+          }
+
+          axios(config).then((res) => {
+            console.log('Axios Response', res)
+          })
+        } else {
+          //Check if Member is Already Admin/Leader
+          console.log('check user metadata')
+          //Check auth0 roles and add roles 'bishopAdmin'
+        }
+      })
     },
   },
 }

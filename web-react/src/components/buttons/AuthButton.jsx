cc5c289dc38cd67b92ed3ec40e06e173ccafd849
John-Dag Addy <jaeadgy@gmail.com>
2021-05-30 08:34:12 +0000

    changed Popup to Popupses

--- web-react/src/components/buttons/AuthButton.jsx
+++ web-react/src/components/buttons/AuthButton.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
-import Popup from '../Popup/Popup'
+import Popup from '../Popupses/Popup'
 
 const AuthButton = (props) => {
   const { loginWithRedirect, logout, isAuthenticated } = useAuth0()

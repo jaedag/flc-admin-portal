7631fb0e62e30bde20ca9fa01340079f694638ce
John-Dag Addy <jaeadgy@gmail.com>
2021-06-09 09:35:16 +0000

    empty

--- web-react/src/components/formik-components/TextError.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function TextError(props) {
-  return <small className="error">{props.children}</small>
-}
-
-export default TextError
--- web-react/src/queries/DeletionMutations.js
+++
@@ -1,7 +0,0 @@
-import { gql } from '@apollo/client'
-
-export const DELETE_BACENTA_MUTATION = gql`
-  mutation DeleteBacenta($id: ID!) {
-    DeleteBacenta(id: $id)
-  }
-`

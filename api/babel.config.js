3a502e03a87e5b2515c0f02cc138c3584d71825a
John-Dag Addy <jaeadgy@gmail.com>
2021-06-10 16:32:05 +0000

    tried fixing npm error with yarn

--- api/babel.config.js
+++ api/babel.config.js
@@ -25,6 +25,7 @@ module.exports = {
       },
     ],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
     [
       '@babel/plugin-transform-runtime',
       {

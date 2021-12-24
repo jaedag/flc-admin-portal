141e9010831d7fbda80e547f378179c42fdcde7f
John-Dag Addy <jaeadgy@gmail.com>
2021-12-24 00:32:59 +0000

    updated all packages except for react-scripts

--- web-react/src/setupProxy.js
+++ web-react/src/setupProxy.js
@@ -1,11 +1,12 @@
-const proxy = require('http-proxy-middleware')
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
   app.use(
     '/graphql',
-    proxy({
+    createProxyMiddleware({
       target: `${process.env.PROXY}`,
       changeOrigin: true,
     })
   )
+  app.listen(3000)
 }

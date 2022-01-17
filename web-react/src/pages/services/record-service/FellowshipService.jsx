b418bc69455dcc570d925defcffb999e86733cff
John-Dag Addy <jaedagy@gmail.com>
2022-01-17 12:03:15 +0000

    added refetch queries in a few places where the cache did not seem to be updating manually

--- web-react/src/pages/services/record-service/FellowshipService.jsx
+++ web-react/src/pages/services/record-service/FellowshipService.jsx
@@ -6,13 +6,16 @@ import { RECORD_SERVICE } from './RecordServiceMutations'
 import { DISPLAY_FELLOWSHIP } from '../../directory/display/ReadQueries'
 import ServiceForm from './ServiceForm'
 import BaseComponent from 'components/base-component/BaseComponent'
+import { FELLOWSHIP_REPORT } from '../reports/ReportQueries'
 
 const FellowshipService = () => {
   const { fellowshipId } = useContext(ChurchContext)
   const { data, loading, error } = useQuery(DISPLAY_FELLOWSHIP, {
     variables: { id: fellowshipId },
   })
-  const [RecordService] = useMutation(RECORD_SERVICE)
+  const [RecordService] = useMutation(RECORD_SERVICE, {
+    refetchQueries: [{ query: FELLOWSHIP_REPORT }],
+  })
 
   return (
     <BaseComponent loading={loading} error={error} data={data}>

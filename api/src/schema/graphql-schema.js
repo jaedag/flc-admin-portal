7b7becc3e7fd6e578ea629fce4faffcbebd12f36
John-Dag Addy <jaedagy@gmail.com>
2022-02-04 12:37:13 +0000

    fix: tried switching import for require in graphql-schema

--- api/src/schema/graphql-schema.js
+++ api/src/schema/graphql-schema.js
@@ -1,6 +1,5 @@
-import fs from 'fs'
-import path from 'path'
-
+const fs = require('fs')
+const path = require('path')
 /*
  * Check for GRAPHQL_SCHEMA environment variable to specify schema file
  * fallback to schema.graphql if GRAPHQL_SCHEMA environment variable is not set

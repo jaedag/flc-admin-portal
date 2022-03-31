56ddcf4fd580cc89d2965755628e7aa84310926d
John-Dag Addy <jaedagy@gmail.com>
2022-03-31 14:52:29 +0000

    fix: fixed create member submission

--- web-react/src/pages/directory/create/CreateMember.jsx
+++ web-react/src/pages/directory/create/CreateMember.jsx
@@ -63,8 +63,8 @@ const CreateMember = () => {
   const navigate = useNavigate()
 
   const onSubmit = async (values, onSubmitProps) => {
-    const { isSubmitting, resetForm } = onSubmitProps
-    isSubmitting(true)
+    const { setSubmitting, resetForm } = onSubmitProps
+    setSubmitting(true)
     // Variables that are not controlled by formik
 
     const pastoralAppointment = filterPastoralTitles(values.pastoralAppointment)
@@ -105,7 +105,7 @@ const CreateMember = () => {
           )
         })
 
-        isSubmitting(false)
+        setSubmitting(false)
         resetForm()
         navigate('/member/displaydetails')
       })

26fc65b6601bbfb547884360a62e3cb53b8dbbbf
John-Dag Addy <jaedagy@gmail.com>
2022-04-06 11:49:13 +0000

    feat: implemented a history log when a member changes fellowships

--- web-react/src/pages/directory/update/UpdateMember.jsx
+++ web-react/src/pages/directory/update/UpdateMember.jsx
@@ -3,7 +3,11 @@ import { useNavigate } from 'react-router-dom'
 import { useQuery, useMutation } from '@apollo/client'
 
 import { parsePhoneNum, throwErrorMsg } from '../../../global-utils'
-import { UPDATE_MEMBER_EMAIL, UPDATE_MEMBER_MUTATION } from './UpdateMutations'
+import {
+  UPDATE_MEMBER_EMAIL,
+  UPDATE_MEMBER_FELLOWSHIP,
+  UPDATE_MEMBER_MUTATION,
+} from './UpdateMutations'
 import {
   DISPLAY_MEMBER_BIO,
   DISPLAY_MEMBER_CHURCH,
@@ -11,8 +15,8 @@ import {
 
 import { MemberContext } from '../../../contexts/MemberContext'
 import MemberForm from '../../../components/reusable-forms/MemberForm'
-import { ADD_MEMBER_TITLE_MUTATION } from 'pages/directory/create/CreateMutations'
-import { filterPastoralTitles } from 'components/reusable-forms/form-utils'
+// import { ADD_MEMBER_TITLE_MUTATION } from 'pages/directory/create/CreateMutations'
+// import { filterPastoralTitles } from 'components/reusable-forms/form-utils'
 
 const UpdateMember = () => {
   const { memberId } = useContext(MemberContext)
@@ -43,6 +47,7 @@ const UpdateMember = () => {
     occupation: member?.occupation?.occupation ?? '',
     pictureUrl: member?.pictureUrl ?? '',
     fellowship: memberChurch?.fellowship ? memberChurch?.fellowship?.name : '',
+    fellowshipName: '',
     ministry: memberChurch?.ministry ? memberChurch?.ministry?.id : '',
 
     pastoralHistory: [
@@ -76,14 +81,15 @@ const UpdateMember = () => {
     ],
   })
   const [UpdateMemberEmail] = useMutation(UPDATE_MEMBER_EMAIL)
-  const [AddMemberTitle] = useMutation(ADD_MEMBER_TITLE_MUTATION)
+  const [UpdateMemberFellowship] = useMutation(UPDATE_MEMBER_FELLOWSHIP)
+  // const [AddMemberTitle] = useMutation(ADD_MEMBER_TITLE_MUTATION)
 
   const onSubmit = async (values, onSubmitProps) => {
     onSubmitProps.setSubmitting(true)
 
     //Variables that are not controlled by formik
 
-    const pastoralAppointment = filterPastoralTitles(values.pastoralAppointment)
+    // const pastoralAppointment = filterPastoralTitles(values.pastoralAppointment)
     try {
       await UpdateMember({
         variables: {
@@ -103,32 +109,44 @@ const UpdateMember = () => {
           ministry: values.ministry,
         },
       })
+      if (initialValues.email !== values.email) {
+        await UpdateMemberEmail({
+          variables: {
+            id: memberId,
+            email: values.email.trim().toLowerCase(),
+          },
+        })
+      }
 
-      await UpdateMemberEmail({
-        variables: {
-          id: memberId,
-          email: values.email.trim().toLowerCase(),
-        },
-      })
+      if (memberChurch?.fellowship.id !== values.fellowship) {
+        await UpdateMemberFellowship({
+          variables: {
+            memberId: memberId,
+            fellowshipId: values.fellowship,
+            ids: [memberId, values.fellowship, memberChurch?.fellowship.id],
+            historyRecord: `${member.firstName} ${member.lastName} moved from ${memberChurch?.fellowship.name} Fellowship to ${values.fellowshipName} Fellowship`,
+          },
+        })
+      }
 
-      pastoralAppointment.forEach(async (title) => {
-        if (!title.date) {
-          return
-        }
+      // pastoralAppointment.forEach(async (title) => {
+      //   if (!title.date) {
+      //     return
+      //   }
 
-        try {
-          await AddMemberTitle({
-            variables: {
-              memberId: memberId,
-              title: title.title,
-              status: true,
-              date: title.date,
-            },
-          })
-        } catch (error) {
-          throwErrorMsg(`There was a problem adding member title`, error)
-        }
-      })
+      //   try {
+      //     await AddMemberTitle({
+      //       variables: {
+      //         memberId: memberId,
+      //         title: title.title,
+      //         status: true,
+      //         date: title.date,
+      //       },
+      //     })
+      //   } catch (error) {
+      //     throwErrorMsg(`There was a problem adding member title`, error)
+      //   }
+      // })
 
       onSubmitProps.setSubmitting(false)
       onSubmitProps.resetForm()

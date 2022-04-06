9729782d7a6f0ebe2b64bdda061fa0af80d3f620
John-Dag Addy <jaedagy@gmail.com>
2022-04-06 03:10:50 +0000

    feat(defaulters): improved loading ux for the defaulters screen

--- web-react/src/pages/services/defaulters/DefaulterInfoCard.jsx
+++ web-react/src/pages/services/defaulters/DefaulterInfoCard.jsx
@@ -10,7 +10,7 @@ const DefaulterInfoCard = ({ defaulter }) => {
     <Card className="text-center" onClick={() => navigate(defaulter.link)}>
       <Card.Header>{defaulter.title}</Card.Header>
       <PlaceholderCustom
-        loading={!defaulter.data}
+        loading={!defaulter.data || defaulter.data === '0'}
         className={`fw-bold large-number pb-3 ${defaulter.color}`}
       >
         <Card.Body className={`fw-bold large-number ${defaulter.color}`}>
